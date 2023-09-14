c8661af1b4960ed7793a691e9f63ebbeb3bdb73d
aydan606 <aydanbarreto@gmail.com>
2023-09-14 16:09:14 -0300

    adding new files

---
+++ ExerciciosTeste/src/CarrinhoDeCompras.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    private List<Item> carrinhoCompras;
+
+    public CarrinhoDeCompras() {
+        this.carrinhoCompras = new ArrayList<>();
+
+    }
+
+    public void adicionarItem(String nome, float preco, int quantidade) {
+        carrinhoCompras.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome) {
+        List<Item> listaRemocao = new ArrayList<>();
+        for(Item e: carrinhoCompras) {
+            if(e.getNome().equalsIgnoreCase(nome))
+                listaRemocao.add(e);
+        carrinhoCompras.removeAll(listaRemocao);
+        }
+    }
+
+    public double calcularValorTotal() {
+        double valorTotalGeral = 0;
+        double valorTotalProduto;
+        for (Item e: carrinhoCompras) {
+            valorTotalProduto = e.getPreco() * e.getQtd();
+            valorTotalGeral += valorTotalProduto;
+        }
+        return valorTotalGeral;
+    }
+
+    public void exibirItens() {
+        System.out.println(carrinhoCompras);
+    }
+
+    public static void main(String[] args) {
+
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+        carrinhoDeCompras.adicionarItem("Detergente", 1.50f, 1);
+        carrinhoDeCompras.adicionarItem("Sabão em Pó", 3.50f, 3);
+        carrinhoDeCompras.exibirItens();
+    }
+
+}
---
+++ ExerciciosTeste/src/Item.java
@@ -0,0 +1,44 @@
+public class Item {
+
+    private String nome;
+    private double preco;
+    private int qtd;
+
+    public Item(String nome, double preco, int qtd) {
+        this.nome = nome;
+        this.preco = preco;
+        this.qtd = qtd;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    @Override
+    public String toString() {
+        return  "{Nome = '" + nome + '\'' +
+                ", Preço = " + preco +
+                ", Quantidade = " + qtd +
+                '}';
+    }
+}
